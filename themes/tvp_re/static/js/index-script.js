567ec84975ac51a8fa1e2750ae5d3ba2c7fd08b7
Damian Enriquez <denriquez@tvpage.com>
2017-05-26 18:41:53 -0700

    first version

--- themes/tvp_re/static/js/index-script.js
+++ themes/tvp_re/static/js/index-script.js
@@ -1,8 +1,13 @@
 var videos = [];
-var videoOffset = 0;
+var currentVideo = null;
 var profiles = [];
 var currentProfile = null;
 var products = [];
+var selectedProducts = [];
+var profileVersion = '';
+var player = null
+var playerDivId = '';
+var apiBaseUrl = '//local.tvpage.com/api';
 
 $( document ).ready(function() {
 	fetchProfiles();
@@ -33,39 +38,102 @@ $( document ).ready(function() {
 		setMatch(true);
 	});
 
+	$('#SudmitProducts').click(function() {
+		setProductsMatch();
+	});
+
+	setPlayer();
+
 });
+
+function setPlayer() {
+	playerDivId = 'TVPlayerHolder';
+	player =  new TVPage.player({
+       'divId': playerDivId,
+       "apiBaseUrl": apiBaseUrl, // Only used for the transcript api call atm, allows to set the host for any api call, for e.x: '//app.tvpage.com';
+        "controls":{
+          active:true,
+          seekBar:{
+          },
+          floater:{
+          }             
+        },
+        'analytics': {
+          tvpa: false
+        },
+       'onReady': function(a,b){
+          resizePlayer();
+       },
+
+       'onStateChange': function(a,b){
+          if (a == 'tvp:media:cued') {
+          	resizePlayer();
+          }
+       },
+
+       'onError': function(e){
+       }
+    });
+}
 
 function fetchProfiles() {
 	$.ajax({
-		url: 'http://local.tvpage.com/api/profiles',
+		url: apiBaseUrl + '/profiles',
 		jsonpCallback: "renderProfiles",
 		dataType: "jsonp",
+		error: function() {
+			$(".spinner-overlay").hide();
+		}
 	});
 }
 
-function setMatch(valid) {
-	$('.tvp-button').attr('disabled','disabled');
-	console.log(currentProfile);
-	console.log(videos[0]);
+function setProductsMatch() {
 	var data = {
-		version: currentProfile.version,
-		videoId: videos[0].id,
-		isMatch: valid,
-		user: "some user"
+		version: profileVersion,
+		videoId: currentVideo.id,
+		products: products,
+		user: localStorage.getItem("username")
 	};
 
 	$.ajax({
-		url: "http://local.tvpage.com/api/profiles/testVideoProfile/" + currentProfile.id,
+		url: apiBaseUrl + "/profiles/testProductRecommendation",
 		type: "POST",
 		crossDomain: true,
 		dataType: 'json',
 		data: JSON.stringify(data)
 	});
-	videoOffset++;
-	fetchViedoProfiles(currentProfile.id, profiles.indexOf(currentProfile), videoOffset);
+	fetchProductRecommendation();
 }
 
-function renderProfiles(data) {
+function setMatch(valid) {
+	$('.tvp-button').attr('disabled','disabled');
+	var data = {
+		version: currentProfile.version,
+		videoId: currentVideo.id,
+		isMatch: valid,
+		user: localStorage.getItem("username"),
+		position: currentVideo._position
+	};
+
+	$.ajax({
+		url: apiBaseUrl + "/profiles/testVideoProfile/" + currentProfile.id,
+		type: "POST",
+		crossDomain: true,
+		dataType: 'json',
+		data: JSON.stringify(data)
+	});
+
+	currentVideo = videos[videos.indexOf(currentVideo) + 1];
+	if (typeof currentVideo !== "undefined") {
+		renderProfileView(currentVideo);
+	} else {
+		alert ("No More Videos");
+		window.location.reload();
+	}
+	
+}
+
+function renderProfiles(data) {	
 	profiles = data;
 	var $tableBody = $('#TVP-table').find('tbody');
 	$.each(data, function(key, profile) {
@@ -87,70 +155,130 @@ function renderProfiles(data) {
 }
 
 function fetchProductRecommendation() {
+	$(".spinner-overlay").show();
 	$.ajax({
-		url: "http://local.tvpage.com/api/profiles/testProductRecommendation",
+		url: apiBaseUrl + "/profiles/testProductRecommendation",
 		jsonpCallback: "renderProductRecommendationsView",
 		dataType: "jsonp",
 		error: function(){
 			alert ("There's no Videos");
 			window.location.reload();
+			$(".spinner-overlay").hide();
 		}
 	});
 }
 
 function fetchViedoProfiles(id, index) {
 	currentProfile = profiles[index];
+	$(".spinner-overlay").show();
 	$.ajax({
-		url: 'http://local.tvpage.com/api/profiles/testVideoProfile/' + id + "?o=" + videoOffset,
+		url: apiBaseUrl + '/profiles/testVideoProfile/' + id,
 		jsonpCallback: "renderVideoProfilesView",
 		dataType: "jsonp",
 		error: function(){
 			alert ("There's no Videos");
 			window.location.reload();
+			$(".spinner-overlay").hide();
 		}
 	});
 }
 
 function renderVideoProfilesView(data) {
+	$(".spinner-overlay").hide();
 	$('.tvp-button').removeAttr('disabled');
 	if (typeof data.videos !== "undefined" && data.videos.length > 0) {
+		
 		videos = data.videos;
 		$('#HeaderTitle').html('Profile Recommedation Engine');
 		$('#ProfilesTable').hide();
 
 		currentVideo = videos[0];
 		renderProfileView(currentVideo);
-		
 	} else {
 		alert ("There's no Videos");
 	}
 }
 
 function renderProfileView(video) {
+	$('.tvp-button').removeAttr('disabled');
 	$("#VideoTitle").html(video.title);
 	$("#ProfileTitle").html(currentProfile.name);
 
+	player.loadVideo(createAsset(video));
+
+	$("#SelectionView").show();
 	$('#VideoProfileView').show();
 }
 
 
 function renderProductRecommendationsView(data) {
 	if (typeof data.video !== "undefined" && typeof data.products !== "undefined" && data.products.length > 0) {
-		videos[0] = data.video;
+		// setPlayer("TVPlayerHolderPR");
+		currentVideo = data.video;
 		products = data.products;
+		profileVersion = data.profileVersion;
 		$('#HeaderTitle').html('Product Recommedation Engine');
 		$('#ProfilesTable').hide();
+		player.loadVideo(createAsset(currentVideo));
 		renderProductView();
 	} else {
 		alert ("There's no Products");
 	}
+	$(".spinner-overlay").hide();
 }
 
 
 function renderProductView() {
-	$("#VideoTitlePR").html(videos[0].title);
+	$("#VideoTitlePR").html(currentVideo.title);
 
+	var $productsGrid = $("#ProductsGrid");
+	var pt = '';
+	$.each(products, function(key, product) {
+		pt += '<div class="pure-u-1-4">';
+        pt += '<div class="product-img-thumb-wrapper ">';
+        pt += '<div id="productId-' + product.id + '" onclick="setProduct(' + key + ')" class="product-img-thumb" style="background-image: url(' + product.imageUrl + ');"></div>';
+        pt += '</div>';
+        pt += '<div class="product-title">' + product.title + '</div>';
+        pt += '</div>';
+		
+		products[key].isMatch = false;
+	});
 
-
+	$productsGrid.html(pt);
+	$("#SelectionView").show();
 	$('#ProductsRecommendationView').show();
+}
+
+function setProduct(index) {
+	var $selected = $('#productId-' + products[index].id);
+	if (!$selected.parent().hasClass('active')) {
+		$selected.parent().addClass('active');
+		products[index].isMatch = true;
+
+	} else {
+		$selected.parent().removeClass('active');
+		products[index].isMatch = false;
+	}
+}
+
+
+function createAsset (obj){
+	if (!obj || "object" !== typeof obj || obj.length == 0 || typeof obj.asset == "undefined") return;
+
+    var asset = obj.asset;
+    asset.assetId = obj.id;
+    asset.assetTitle = obj.title;
+    asset.loginId = obj.loginId;
+
+
+    if (!asset.sources) asset.sources = [{ file: asset.videoId }];
+   	asset.type = asset.type || 'youtube';
+
+    return asset;
+};
+
+
+
+function resizePlayer() {
+	player.resize($('#' + playerDivId).width(), document.getElementById(playerDivId).offsetHeight);
 }
