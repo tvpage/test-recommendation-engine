7ef304cb1f823ff8e1f113804cef9bdb54a250ac
Ben van Klinken <ben@villagechief.com>
2017-06-27 09:48:13 +1000

    fix

--- themes/tvp_re/static/js/index-script.js
+++ themes/tvp_re/static/js/index-script.js
@@ -321,7 +321,7 @@ function setProfilesMatch() {
 	var data = {
 		videoId: currentVideo == null ? null : currentVideo.id,
 		productId: currentProduct == null ? null : currentProduct.id,
-		loginId: (currentVideo == null ? currentProduct.loginId : (currentVideo == null ? $("#loginId").val().trim() : currentVideo.loginId)),
+		loginId: (currentVideo == null ? (currentProduct == null ? $("#loginId").val().trim() : currentProduct.loginId)),
 		profiles: otherProfiles,
 		user: localStorage.getItem("username")
 	};
